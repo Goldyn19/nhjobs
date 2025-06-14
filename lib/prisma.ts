7d7cd69c411c787a6142fe5ea3f0d1603706d040
Goldyn19 <nwaezegolden2002@gmail.com>
2025-06-14 11:07:20 +0100

    change var declaration in prisma.ts file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,10 +4,12 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma 
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
